df063641a879f768ffa3cd38c6e9ec7ff0fc4746
roman <romanzchov@mail.ru>
2024-04-29 15:34:22 +0300

    3 finish

--- src/main/java/org/example/integer/IntegerListImpl.java
+++ src/main/java/org/example/integer/IntegerListImpl.java
@@ -28,6 +28,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(Integer item) {
+        grow();
         data[size] = item;
         size++;
         return item;
@@ -35,7 +36,9 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(int index, Integer item) {
+
         checkIndex(index);
+        grow();
         for (int i = size; i >= index; i--) {
             data[i + 1] = data[i];
         }
@@ -98,6 +101,7 @@ public class IntegerListImpl implements IntegerList {
         }
         return false;
     }
+
 
     @Override
     public int indexOf(Integer item) {
@@ -190,6 +194,31 @@ public class IntegerListImpl implements IntegerList {
         }
 
         return arr;
+    }
+
+    public  void quickSort(Integer[] arr, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+
+            quickSort(arr, begin, partitionIndex - 1);
+            quickSort(arr, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
     }
 
 
